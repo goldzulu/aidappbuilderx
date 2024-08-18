fc2cf155626d6a290da57561b90282794433e38a
goldzulu <goldzulu222@gmail.com>
2024-08-18 11:41:51 +0100

    NOTE this is only to make streamlit works. remove first two lines for local install from app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-import sys
+# WARNING: The following two lines are ONLY for Streamlit.
+# Remove them from local install!!
+__import__('pysqlite3')
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import os
+import sys
 
 # from embedchain import App
 from embedchain.pipeline import Pipeline as App
+
 from embedchain.loaders.github import GithubLoader
 
 @st.cache_resource
